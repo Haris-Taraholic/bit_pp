eb5b074eab716c9ec23a899a3337cd56550eaf60
BIT Student <classroom@bgit.rs>
2020-02-26 17:59:45 +0100

    OBJEKTI

---
+++ exercise/practice2.js
@@ -0,0 +1,68 @@
+// IZBACI SVE BROJEVE KOJI SU VECI OD 4, ALI SAMO BROJEVI SMEJU U NIZ DA OSTANU
+
+// var a = [3, 7, -1, 6, undefined, 4, "bla", 5, 2];
+// var b = [];
+// for (var i = 0; i < a.length; i++) {
+//     if (a[i] < 4 && typeof a[i] === "number") {
+//         b[b.length] = a[i];
+//     }
+// }
+// console.log(b);
+
+///////////////////////////TASK: 1///////////////////////////////////
+
+// function positionJS(string, n) {
+//     var newString = "";
+//     var position = n === undefined ? 0 : n;
+
+//     for (var i = 0; i < string.length; i++) {
+//         if (i === position) {
+//             newString += "JS ";
+//         }
+//         newString += string[i];
+//     }
+//     return newString;
+// }
+
+// var a = "My random String";
+// var b;
+
+// var result = positionJS(a, b);
+// console.log(result);
+
+///////////////////////////TASK: 2///////////////////////////////////
+
+// function isNumber(array) {
+//     var newString = "";
+
+//     for (var i = 0; i < a.length; i++) {
+//         if (isFinite(a[i]) && !isNaN(a[i]) && typeof a[i] === "number") {
+//             newString += a[i];
+//         }
+//     }
+//     return newString;
+// }
+
+// var a = [NaN, 0, 15, false, 22, '', undefined, 47, null];
+// var result = isNumber(a);
+// console.log(result);
+
+///////////////////////////TASK: 3///////////////////////////////////
+
+// function filter(array) {
+//     var b = [];
+//     for (var i = 0; i < array.length; i++) {
+//         if (!!a[i] !== false) {
+//             b[b.length] = a[i];
+//         }
+//     }
+//     return b;
+// }
+
+// var a = [NaN, 0, 15, false, 22, '', undefined, 47, null];
+// var result = filter(a);
+
+// console.log(result);
+
+///////////////////////////TASK: 4///////////////////////////////////
+
